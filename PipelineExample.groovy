06dc564a4694dd33041ec459bf9716040d86ef3b
jne <jnertl@outlook.com>
2025-08-29 10:05:16 +0300

    Add Jenkins pipeline for middlewaresw project build and testing

---
+++ PipelineExample.groovy
@@ -0,0 +1,44 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                sh """
+                    rm -rf middlewaresw
+                    git clone --single-branch --branch main https://github.com/jnertl/middlewaresw.git
+                """
+            }
+        }
+        stage('Build') {
+            steps {
+                sh """
+                    cd middlewaresw
+                    bash ./build.sh
+                """
+            }
+        }
+        stage('Unit tests') {
+            steps {
+                sh """
+                    cd middlewaresw
+                    bash ./run_tests.sh
+                """
+            }
+        }
+        stage('Coverage report') {
+            steps {
+                sh """
+                    cd middlewaresw
+                    bash ./run_coverage.sh
+                    zip -r coverage_html.zip coverage_html
+                """
+            }
+        }
+    }
+    post {
+        always {
+            archiveArtifacts artifacts: 'middlewaresw/coverage_html.zip', fingerprint: true
+        }
+    }    
+}
